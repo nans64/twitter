b9274373d2c3ce3b52d2e0b2751a49f7fb940039
nans <nans.noel@gmail.com>
2019-01-16 16:59:49 +0100

    ADD

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ ruby '2.5.1'
 gem 'rspec'
 gem 'pry'
 gem 'rubocop', '~> 0.57.2'
-gem 'dotenv'
 require 'pry'
+gem 'dotenv'
+
--- lib/preuve.rb
+++ lib/preuve.rb
@@ -4,6 +4,5 @@ Dotenv.load # Ceci appelle le fichier .env grâce à la gem Dotenv, et importe t
 
 # Il est ensuite très facile d'appeler les données du fichier .env, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
 puts ENV['TWITTER_API_SECRET']
-
+puts "je ne marche pas"
 # tout est stocké dans un hash qui s'appelle ENV. Tu peux le regarder en faisant :
-puts ENV
